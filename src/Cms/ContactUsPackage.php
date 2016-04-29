e3614cbbd9e0737f96e40f389248a1f93a070c22
Elliot Levin <elliot@iddigital.com.au>
2016-04-29 14:27:13 +1000

    Add boot method to package and bind repository

--- src/Cms/ContactUsPackage.php
+++ src/Cms/ContactUsPackage.php
@@ -2,14 +2,27 @@
 
 namespace Dms\Package\ContactUs\Cms;
 
+use Dms\Core\ICms;
+use Dms\Core\Ioc\IIocContainer;
 use Dms\Core\Package\Definition\PackageDefinition;
 use Dms\Core\Package\Package;
+use Dms\Package\ContactUs\Core\IContactEnquiryRepository;
+use Dms\Package\ContactUs\Persistence\DbContactEnquiryRepository;
 
 /**
  * @author Elliot Levin <elliotlevin@hotmail.com>
  */
 class ContactUsPackage extends Package
 {
+    /**
+     * @param ICms $cms
+     *
+     * @return void
+     */
+    public static function boot(ICms $cms)
+    {
+        $cms->getIocContainer()->bind(IIocContainer::SCOPE_SINGLETON, IContactEnquiryRepository::class, DbContactEnquiryRepository::class);
+    }
 
     /**
      * Defines the structure of this cms package.

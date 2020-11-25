759b94c10f58f1e1bb1430610b3f9fcb0f25dd88
Ali Hamza <ali.hamza@gmail.com>
2020-11-25 14:57:35 +1100

    DMS Upgrade

--- tests/Tests/Core/ContactEnquiryServiceTest.php
+++ tests/Tests/Core/ContactEnquiryServiceTest.php
@@ -24,7 +24,7 @@ class ContactEnquiryServiceTest extends CmsTestCase
      */
     protected $service;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
